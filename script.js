3a1878a5c46eb2a79b10b7d4a3a1d220e70d365b
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-07-16 19:31:06 +0530

    btn-container added

--- script.js
+++ script.js
@@ -2,5 +2,14 @@ var addTodo = document.getElementById('addTask');
 var writeTodo = document.getElementById('todo');
 addTodo.addEventListener('click', function(){
     writeTodo.setAttribute('rows', 3)
-    document.querySelector('.task-container')
+    writeTodo.setAttribute('placeholder',"Enter Task title here")
+    addTodo.style.display = 'none';
+    document.querySelector('.task-container').style.alignItems = 'flex-start'
+    document.querySelector('.btn-container').style.opacity='1'
 })
+
+function cancel(){
+    document.querySelector('.btn-container').style.opacity='0'
+    writeTodo.setAttribute('rows', 1)
+    addTodo.style.display = 'block';
+}
